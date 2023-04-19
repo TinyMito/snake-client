dc4bdf4b39f6214b835df406ae5330a81b770c37
TinyMito <k.ip@me.com>
2023-04-19 01:00:07 -0400

    Update to support client.js import

--- play.js
+++ play.js
@@ -1,19 +1,4 @@
 const net = require('net');
+const connect = require('./client')
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  conn.setEncoding('utf-8');
-
-  conn.on('data', (data) => {
-    console.log(data)
-  });
-
-  return conn;
-};
-
-console.log('Connecting ...');
 connect();
