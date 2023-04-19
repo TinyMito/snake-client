8bf4d6112a9bfd979ca0e35f64779156e9d22da8
TinyMito <k.ip@me.com>
2023-04-19 01:22:11 -0400

    Test movement

--- client.js
+++ client.js
@@ -15,6 +15,18 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write('Name: KIP');
+/*    setInterval(function(){
+      conn.write('Move: up');
+      conn.write('Move: up');
+      conn.write('Move: left');
+      conn.write('Move: left');
+      conn.write('Move: down');
+      conn.write('Move: down');
+      conn.write('Move: right');
+      conn.write('Move: right');
+    }, 50)   
+*/  
+    
   });
 
   return conn;
