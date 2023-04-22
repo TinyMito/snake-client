8ac63d3b61a39543afe40cafdb586cbde9ec5339
TinyMito <k.ip@me.com>
2023-04-21 21:22:31 -0400

    Update message with constant and clean up with eslint

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const net = require('net');
-const { IP, PORT } = require("./constants");
+const { IP, PORT, endMessage, connectMessage, closeMessage, playerName } = require("./constants");
 
 
 const connect = function() {
@@ -11,19 +11,22 @@ const connect = function() {
 
   conn.setEncoding('utf-8');
 
+  // On server send data
   conn.on('data', (data) => {
-    console.log(data)
+    console.log(data);
+    console.log(endMessage);
   });
 
   // On server connect
   conn.on('connect', () => {
-    console.log('Successfully connected to the game server!');
-    conn.write('Name: KIP');
+    console.log(connectMessage);
+    conn.write(playerName);
   });
 
-  // On server close
+  // On server close or disconnect player
   conn.on('close', () => {
-    console.log('You have been disconnected from the game server!');
+    console.log(closeMessage);
+    process.exit();
   });
 
   return conn;
