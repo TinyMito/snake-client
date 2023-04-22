f92015b84bae65e106d74dad9f45d9639f0ff93e
TinyMito <k.ip@me.com>
2023-04-21 21:24:42 -0400

    Add constants values

--- constants.js
+++ constants.js
@@ -1,7 +1,35 @@
 const IP = 'localhost';
 const PORT = 50541;
+const connectInitial = 'Connecting ...';
+const connectMessage = 'Successfully connected to the game server!';
+const closeMessage = 'You have been disconnected from the game server!';
+const endMessage = 'Game over!';
+const playerName = 'Name: KIP';
+const key1Chat = 'Say: Welcome!';
+const key2Chat = 'Say: Hello!';
+const key3Chat = 'Say: Goodbye!';
+const key4Chat = 'Say: Congratulation!';
+const wBind = 'Move: up';
+const aBind = 'Move: left';
+const sBind = 'Move: down';
+const dBind = 'Move: right';
+const exitBind = '\u0003';
 
 module.exports = {
   IP,
   PORT,
+  connectInitial,
+  connectMessage,
+  closeMessage,
+  endMessage,
+  playerName,
+  key1Chat,
+  key2Chat,
+  key3Chat,
+  key4Chat,
+  wBind,
+  aBind,
+  sBind,
+  dBind,
+  exitBind
 };
